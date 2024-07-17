754e40a534c0cf2d3bd158033655c394899dcb41
suchawadeep <suchawadeenuna@gmail.com>
2024-07-17 20:15:51 +0700

    init: draft project structure

---
+++ admin-dashboard-service/src/test/java/com/ecommerce/admin_dashboard_service/AdminDashboardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.admin_dashboard_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminDashboardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
