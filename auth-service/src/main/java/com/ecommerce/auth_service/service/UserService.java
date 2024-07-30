29d869ef5bb80b5e4d55cc7b83088c0e141c213e
suchawadeep <suchawadeenuna@gmail.com>
2024-07-30 21:38:54 +0700

    imp: auth service logic

--- auth-service/src/main/java/com/ecommerce/auth_service/service/UserService.java
+++ auth-service/src/main/java/com/ecommerce/auth_service/service/UserService.java
@@ -2,14 +2,15 @@ package com.ecommerce.auth_service.service;
 
 import com.ecommerce.auth_service.dto.JsonResponse;
 import com.ecommerce.auth_service.dto.UserIdResponse;
+import com.ecommerce.auth_service.dto.UserResponse;
 import com.ecommerce.auth_service.exception.UserNotFoundException;
 import com.ecommerce.auth_service.model.User;
+import com.ecommerce.auth_service.model.Role;
 import com.ecommerce.auth_service.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import org.springframework.transaction.annotation.Transactional;
-
 
 @Service
 @Transactional
@@ -20,26 +21,44 @@ public class UserService {
 
     public JsonResponse<UserIdResponse> getUserById(Long id) {
         User user = userRepository.findByUserId(id);
-        UserIdResponse myResult = new UserIdResponse(user.getUsername(), user.getEmail());
+        if (user == null) {
+            throw new UserNotFoundException("-1","User not found");
+        }
+        UserIdResponse myResult = new UserIdResponse(user.getId(), user.getUsername(), user.getEmail());
         return new JsonResponse<>("0", "successful", null, myResult);
     }
 
-    public JsonResponse<Void> login(String email, String password) {
+    public JsonResponse<UserResponse> login(String email, String password) {
         try {
             User user = userRepository.findByEmail(email);
-            if (user.getPassword().equals(password)) {
+            if (user != null && user.getPassword().equals(password)) {
                 userRepository.updateLastLogin(user.getId(), LocalDateTime.now());
-                System.out.println("Login successful");
-                return new JsonResponse<>("0", "Login successful");
+                Role role = getRolesByUserId(user.getId());
+                UserResponse myResult = new UserResponse(user.getId(), role.getName());
+
+                return new JsonResponse<>("0", "successful", null, myResult);
             } else {
-                System.out.println("Login failed");
-                return new JsonResponse<>("-1", "Login failed");
+                return new JsonResponse<>("-1", "Login failed",null,null);
             }
         } catch (UserNotFoundException e) {
-            return new JsonResponse<>("-1", e.getMessage());
+            return new JsonResponse<>("-1", e.getMessage(),null,null);
         } catch (Exception e) {
             e.printStackTrace();
-            return new JsonResponse<>("-1", "An unexpected error occurred");
+            return new JsonResponse<>("-1", "An unexpected error occurred",null,null);
+        }
+    }
+
+    private Role getRolesByUserId(Long userId) {
+        try {
+            Role role = userRepository.findRolesByUserId(userId);
+            if (role == null) {
+                throw new UserNotFoundException("-1","No roles found for user");
+            }
+            return role;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            throw new UserNotFoundException("-1","An unexpected error occurred durling getRoleByUserId");
         }
     }
 
