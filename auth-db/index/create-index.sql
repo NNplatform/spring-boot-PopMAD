8b359f8617946546a0ab40dd1f6323d360b95fe6
suchawadeep <suchawadeenuna@gmail.com>
2024-07-30 21:36:16 +0700

    imp: schema scripts for auth database

--- auth-db/index/create-index.sql
+++ auth-db/index/create-index.sql
@@ -1,2 +1,2 @@
 CREATE INDEX IF NOT EXISTS idx_users_id ON users (id);
-CREATE INDEX IF NOT EXISTS idx_user_roles_role_id ON student_profile (role_id);
+CREATE INDEX IF NOT EXISTS idx_user_roles_role_id ON user_roles (role_id);
--- auth-db/readme.txt
+++ auth-db/readme.txt
@@ -1,2 +1,3 @@
+docker build --no-cache -t auth-database .
 docker volume create auth-db-postgres
 docker run -dp 5600:5432 -v auth-db-postgres:/var/lib/postgresql/data auth-database
